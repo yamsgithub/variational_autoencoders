ffb2818a8ae85ae8564aedad23eceb14d6a7dec0
Yamuna Krishnamurthy <yamunak@gmail.com>
2020-01-20 13:38:13 +0000

    return the mean of the loss

--- utils.py
+++ utils.py
@@ -5,9 +5,6 @@ from tensorflow.keras.layers import Flatten
 def sampling(args):
     z_mean, z_log_sigma = args
     epsilon = K.random_normal(shape = (z_mean.get_shape().as_list()[1],) , mean = 0, stddev = 1)
-    #print('EPSILON ', epsilon,'\n',epsilon.shape,' \n MEAN', K.get_value(K.mean(epsilon)))
-    print('\n\n\nZ MEAN ', Flatten()(z_mean))
-    print('Z LOG SIGMA ', Flatten()(z_log_sigma),'\n\n\n')
     return z_mean + K.exp(z_log_sigma/2) * epsilon
 
 def vae_loss(z_mean, z_log_sigma):
@@ -19,7 +16,7 @@ def vae_loss(z_mean, z_log_sigma):
         # Compute the KL Divergence regularization term
         kl_loss = - 0.5 * K.sum(1 + z_log_sigma - K.square(z_mean) - K.exp(z_log_sigma), axis = -1)
         # Return the average loss over all images in batch
-        total_loss = (reconstruction_loss + 0.0001 * kl_loss)
+        total_loss = K.mean(reconstruction_loss + 0.0001 * kl_loss)
         return total_loss
     return loss
 
